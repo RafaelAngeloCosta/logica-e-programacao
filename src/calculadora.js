bd9ee6d4eb490e74efa111a900f2106460844672
@RafaelAngeloCosta <rafa.angelo9@gmail.com>
2025-08-28 23:13:22 -0300

    Adicionei a soma e seu teste

---
+++ src/calculadora.js
@@ -0,0 +1,20 @@
+function somarDoisNumeros(valor1,valor2) {
+   const resultado = valor1 + valor2;
+   return resultado;
+}
+
+function calcularMediaDeDoisNumeros(valor1, valor2){
+    // soma todos os valores;
+    const resultadoSomaDeDoisValores = somarDoisNumeros(valor1 ,valor2);
+    // divide pela quantidade de números passados;
+    const resultadoDaMediaDeDoisValores = resultadoSomaDeDoisValores / 2;
+    // Retornar o resultado.
+    return resultadoDaMediaDeDoisValores;
+
+
+}
+
+
+module.exports = {
+    somarDoisNumeros
+}
